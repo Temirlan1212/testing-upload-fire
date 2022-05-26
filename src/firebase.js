33bd6ccbcc8da52f119eb6b2a90fd13da54db26a
TEMIRLAN <97175881+Temirlan1212@users.noreply.github.com>
2022-05-26 07:26:00 +0300

    upload-image-added

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCe8-IJKGUd-yXoYR0xQKMdSuIv5YsznDE",
-  authDomain: "fileuploads-be0db.firebaseapp.com",
-  projectId: "fileuploads-be0db",
-  storageBucket: "fileuploads-be0db.appspot.com",
-  messagingSenderId: "832032829361",
-  appId: "1:832032829361:web:f2079f619d4aad32ae9790",
+  //*Поставьте свои конфиги
 };
 
 // Initialize Firebase
